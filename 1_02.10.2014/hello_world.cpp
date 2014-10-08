d99f492c52d2173a6665b12a1421312471253740
kakato10 <svetimir.ignatov@gmail.com>
2014-10-08 22:49:45 +0300

    Making some changes to the "Hello World" program

--- 1_02.10.2014/hello_world.cpp
+++ 1_02.10.2014/hello_world.cpp
@@ -1,8 +1,11 @@
 #include <iostream>  //directive to the preprocessor which asks him to include the library iostream
-using namespace std; //telling the compiler the we are going to use the standart 
-					 //namespace where "cout" is stored
+using namespace std; /*telling the compiler the we are going to use the standart 
+					   namespace where "cout" is stored*/
 
 int main() {
 	cout << "Hello world!!!" << endl;
+	cin.get(); 
+	/* system("pause") is not considered to be a good practice, so we will use cin.get() in order to stop
+	our program and see it's output */
 	return 0;
 }
