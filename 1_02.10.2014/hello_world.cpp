bfc3407dbcc8bb22c350dd362575100affce9d57
kakato10 <svetimir.ignatov@gmail.com>
2014-10-08 13:46:05 +0300

    Adding materials from the first Introduction to Programming exercise

---
+++ 1_02.10.2014/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  //directive to the preprocessor which asks him to include the library iostream
+using namespace std; //telling the compiler the we are going to use the standart 
+					 //namespace where "cout" is stored
+
+int main() {
+	cout << "Hello world!!!" << endl;
+	return 0;
+}
